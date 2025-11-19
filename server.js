f5401bdecb7f68d3a41595b357171291e347e58e
kevindelaguila <72113098+kevindelaguila@users.noreply.github.com>
2025-11-19 17:53:47 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const cors = require("cors");
-const request = require("request");
+const fetch = require("node-fetch");
+const { pipeline } = require("stream");
 
 const app = express();
 app.use(cors());
 
-app.get("/radio", (req, res) => {
-  request("http://juventudpalabramiel.org:8000/radio").pipe(res);
+app.get("/radio", async (req, res) => {
+  try {
+    const radioResponse = await fetch("http://juventudpalabramiel.org:8000/radio.mp3");
+
+    if (!radioResponse.ok) {
+      return res.status(500).send("Error al obtener el stream");
+    }
+
+    res.setHeader("Content-Type", "audio/mpeg");
+
+    pipeline(radioResponse.body, res, (err) => {
+      if (err) {
+        console.error("Pipeline error:", err);
+        res.status(500).end();
+      }
+    });
+
+  } catch (err) {
+    console.error("Fetch error:", err);
+    res.status(500).send("Error interno del servidor");
+  }
 });
 
 const PORT = process.env.PORT || 3000;
